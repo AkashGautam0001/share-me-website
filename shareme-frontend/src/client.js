07b9db767fb9ac66dcbcb66b9eb170486d23f7bb
Akash Gautam <akashgautam01584@gmail.com>
2024-04-14 09:31:12 +0530

    production name added.

--- shareme-frontend/src/client.js
+++ shareme-frontend/src/client.js
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
 	projectId: import.meta.env.VITE_APP_SANITY_STUDIO_PROJECT_ID,
-	dataset: "production",
+	dataset: import.meta.env.VITE_APP_SANITY_STUDIO_PRODUCTION,
 	apiVersion: "2024-04-13",
 	useCdn: true,
 	token: import.meta.env.VITE_APP_SANITY_STUDIO_TOKEN,
